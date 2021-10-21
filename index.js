76a2e2facd49d5c4daa7b35739b19599d34e9e89
Marcio Oliveira <marcio.oliveira@tradersclub.com.br>
2021-10-21 13:00:38 -0400

    auto start

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const port = 12000;
 const express = require('express')
+const open = require('open');
 const app = express();
 
 app.use(express.static('app'))
 
 app.listen(port, () => {
     console.log("Listening",port)
+    open(`http://localhost:${port}`)
 })
